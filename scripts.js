9b5bc82a8b09443367636eda02ac555d35783be1
Mary Niederschmidt <mary.niederschmidt@gmail.com>
2021-06-02 16:46:07 -0500

    Added some additional basic code to get started

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init () {
+    const shuttle = document.getElementById("rocket");
+
+    shuttle.style.position = "absolute";
+    shuttle.style.left ="0px";
+    shuttle.style.bottom = "0px"; 
+
+    
+}
+
+window.addEventListener("load", init);
